24cef63e10ac78fab56e3bf8c5d204c128dcc450
bader292 <74805231+bader292@users.noreply.github.com>
2025-08-08 16:15:53 +0300

    Create BasicStrategy.py

---
+++ user_data/strategies/BasicStrategy.py
@@ -0,0 +1,31 @@
+# user_data/strategies/BasicStrategy.py
+
+from freqtrade.strategy.interface import IStrategy
+from pandas import DataFrame
+
+class BasicStrategy(IStrategy):
+    minimal_roi = {
+        "0": 0.05
+    }
+
+    stoploss = -0.1
+    timeframe = '5m'
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[:, 'buy'] = 0
+        dataframe.loc[
+            (dataframe['close'] > dataframe['open']),
+            'buy'
+        ] = 1
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[:, 'sell'] = 0
+        dataframe.loc[
+            (dataframe['close'] < dataframe['open']),
+            'sell'
+        ] = 1
+        return dataframe
